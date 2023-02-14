4f18175e968a4408048cbd477c6ac008c182e710
Digvijay Singh Katoch <digvijaykatoch.application@gmail.com>
2023-02-14 23:25:04 +0530

    Updated app.js

--- static/app.js
+++ static/app.js
@@ -1,1 +1,53 @@
-//To be added soon
+const chatHistory = document.getElementById("chat-history");
+
+    const chatForm = document.getElementById("chat-form");
+
+    const messageInput = document.getElementById("message");
+
+ 
+
+    chatForm.addEventListener("submit", (event) => {
+
+      event.preventDefault();
+
+      const message = messageInput.value;
+
+      messageInput.value = "";
+
+      addChatBubble(message, true);
+
+     fetch('http://localhost:5000/api/send-message', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Origin': 'http://localhost:63342'
+  },
+  body: JSON.stringify({ message: 'Hello, world!' })
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch((error) => console.error(error));
+
+    });
+
+ 
+
+    function addChatBubble(text, isUser) {
+
+      const chatBubble = document.createElement("div");
+
+      chatBubble.classList.add("chat-bubble");
+
+      if (isUser) {
+
+        chatBubble.classList.add("user");
+
+      }
+
+      chatBubble.textContent = text;
+
+      chatHistory.appendChild(chatBubble);
+
+      chatHistory.scrollTop = chatHistory.scrollHeight;
+
+    }
